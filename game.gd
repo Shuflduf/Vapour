45830df887330886c58146fa1bc457fcca6757d7
Shuflduf <shuflduf@gmail.com>
2024-08-09 17:35:16 -0600

    good game base

--- game.gd
+++ game.gd
@@ -1,17 +1,21 @@
 extends Control
 
-@export var pic_size = 128
-@onready var label: Control = $Control
+@export var pic_size = 144
+@onready var label: Control = $LabelContainer/Label
 
 var label_visible = false
 
-func _on_mouse_entered() -> void:
+func _on_label_container_mouse_entered() -> void:
+	print("Enter")
 	label_visible = true
-	var tween = get_tree().create_tween()
+	var tween = get_tree().create_tween().set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_OUT)
 	tween.tween_property(label, "position:x", pic_size, 0.3)
 
 
-func _on_mouse_exited() -> void:
+func _on_label_container_mouse_exited() -> void:
+	print("Leave")
 	label_visible = false
-	var tween = get_tree().create_tween()
+	var tween = get_tree().create_tween().set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_IN)
 	tween.tween_property(label, "position:x", 0, 0.3)
+
+
