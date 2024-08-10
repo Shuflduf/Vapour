6aad1484ce056a4956c0a43ba180eb8ccfaedf37
Shuflduf <shuflduf@gmail.com>
2024-08-10 16:19:42 -0600

    it looks like its working perfectly

--- scripts/main.gd
+++ scripts/main.gd
@@ -15,20 +15,19 @@ func _on_new_game_pressed() -> void:
 
 func add_game(new_entry: GameEntry):
 	new_entry.reparent(games)
+
 	save_games()
-	load_games()
+	connect_all_children()
 
 func edit_game(new_entry: GameEntry, index):
-	print(games.get_child(index))
 	games.get_child(index).free()
 	new_entry.reparent(games)
 	games.move_child(games.get_child(-1), index)
 	save_games()
-	load_games()
+	connect_all_children()
 	#games.get_child(index) = new_entry
 
 func save_games():
-	print("Save")
 	var save_file = FileAccess.open("user://games.json", FileAccess.WRITE)
 	for game in games.get_children():
 		if !game is GameEntry:
@@ -37,10 +36,10 @@ func save_games():
 		save_file.store_line(JSON.stringify(game_data))
 
 func load_games():
-	print("Load")
-
 	for game in games.get_children():
 		game.queue_free()
+
+	await get_tree().process_frame
 
 	var save_file = FileAccess.open("user://games.json", FileAccess.READ)
 	while save_file.get_position() < save_file.get_length():
@@ -58,6 +57,7 @@ func load_games():
 
 		# Get the data from the JSON object
 		var node_data = json.get_data()
+		print(node_data["name"])
 
 		# Firstly, we need to create the object and add it to the tree and set its position.
 		var new_object = game_entry.instantiate()
@@ -65,13 +65,15 @@ func load_games():
 		for i in node_data.keys():
 			if i == "border_colour":
 				new_object.set(i, str_to_var(node_data[i]))
-			if i == "name":
-				new_object.name = node_data[i]
 			else:
 				new_object.set(i, node_data[i])
 
 		games.add_child(new_object, true)
 
+	call_deferred("connect_all_children")
+
+
+func connect_all_children():
 	for i in games.get_children().size():
 
 		games.get_children()[i].tree_exited.connect(save_games)
@@ -80,9 +82,8 @@ func load_games():
 			var new_entry = new_game.instantiate()
 			add_child(new_entry)
 			new_entry.game.queue_free()
-			new_entry.h_box.add_child(games.get_children()[i - 1].duplicate(), true)
+			new_entry.h_box.add_child(games.get_children()[i].duplicate(), true)
 			new_entry.game = new_entry.h_box.get_child(-1)
 			new_entry.update_from_game()
 			new_entry.added_game.connect(func(g: GameEntry):
-				edit_game(g, i - 1)))
-
+				edit_game(g, i)))
