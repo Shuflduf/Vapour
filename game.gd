493eb971c1e25f65a54c393aa96df13e0ab955fb
Shuflduf <shuflduf@gmail.com>
2024-08-09 18:13:53 -0600

    labels work properly

--- game.gd
+++ game.gd
@@ -9,22 +9,20 @@ var tween: Tween
 func tween_label(out: bool):
 	if tween:
 		await tween.finished
-		tween = null
 
 	tween = get_tree().create_tween().set_trans(Tween.TRANS_CIRC)
 	tween.tween_property(label, "position:x", pic_size * int(out), 0.3)\
 			.set_ease(Tween.EASE_OUT if out else Tween.EASE_IN)
 
+	tween.finished.connect(func():
+		tween = null)
+
 
 func _on_label_container_mouse_entered() -> void:
-	print("Enter")
 	label_visible = true
 	tween_label(true)
 
 
 func _on_label_container_mouse_exited() -> void:
-	print("Leave")
 	label_visible = false
 	tween_label(false)
-
-
